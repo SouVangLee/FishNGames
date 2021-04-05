dea15b8aa10ac7c09407901bbcf887f81cef9589
Sou Lee <Sou_Lee92@yahoo.com>
2021-04-05 14:56:11 -0700

    Write session controller methods

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,27 @@
+class Api::SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      login(@user)
+      #render
+    else
+      render json: ["Invalid username/password combination"], status: 401
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      logout
+      #render
+    else
+      render json: ["No one is signed in"], status: 404
+    end
+  end
+
+end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,3 +1,20 @@
 class Api::UsersController < ApplicationController
-  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      #render
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  def show
+
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
