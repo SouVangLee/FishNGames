7381ab76f102f19c38dd9fc46ca3880c01491241
Sou Lee <Sou_Lee92@yahoo.com>
2021-04-05 14:02:34 -0700

    Add methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+
+  def login(user)
+    session[:session_token] = user.reset_session_token!
+    @current_user = user;
+  end
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_logged_in
+    unless current_user
+      render json: { base: ['invalid credentials'] }, status: 401
+    end
+  end
+
+  def logout
+    current_user.reset_session_token!
+    session[:session_token] = nil
+    @current_user = nil
+  end
 end
