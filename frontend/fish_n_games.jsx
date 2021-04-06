b15dc1021cb3c77ef2b133e977b9d670d315f89c
Sou Lee <Sou_Lee92@yahoo.com>
2021-04-05 17:17:29 -0700

    Add store

--- frontend/fish_n_games.jsx
+++ frontend/fish_n_games.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { signup, login, logout } from "./util/session_api_util";
+import configureStore from "./store/store";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
+  const store = configureStore();
+  window.store = store;
   window.signup = signup;
   window.login = login;
   window.logout = logout;
