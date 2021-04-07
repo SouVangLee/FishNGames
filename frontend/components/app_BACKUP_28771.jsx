13a6fe9c1ec976d35c3ce2ea0ee1418b1bb7c5dd
Sou Lee <Sou_Lee92@yahoo.com>
2021-04-06 23:39:22 -0700

    merge conflict

---
+++ frontend/components/app_BACKUP_28771.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import SessionFormContainer from './session/signup_form_container'
+import LoginFormContainer from './session/login_form_container';
+import ModalContainer from "./modal/modal_container";
+import GreetingContainer from './navbar/greeting_container';
+import Greeting from './navbar/greeting';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+
+const App = () => (
+  <div>
+    <ModalContainer />
+    <h1>Fish N Games</h1>
+    <header>
+      <GreetingContainer />
+    </header>
+    <ProtectedRoute exact path="/cart" component={LoginFormContainer} />
+    <AuthRoute path="/signup" component={SessionFormContainer} />
+  </div>
+);
+
+export default App;
