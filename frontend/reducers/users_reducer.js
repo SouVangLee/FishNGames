8ba7aa90f1b45d753e717654a132ddc8090ade3f
Sou Lee <Sou_Lee92@yahoo.com>
2021-04-06 18:17:41 -0700

    Fixed users_reducer so session will now save

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -2,12 +2,9 @@ import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
 
 const usersReducer = (state = {}, action) => {
   Object.freeze(state);
-  const nextState = Object.assign(state);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
-      nextState[action.currentUser.id] = action.currentUser
-      return nextState;
-
+      return Object.assign({}, state, { [action.currentUser.id]: action.currentUser })
     default:
       return state;
   }
