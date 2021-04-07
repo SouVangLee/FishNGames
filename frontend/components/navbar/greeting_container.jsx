0a5364264ebca550dcbc961f6dcc6472112042f9
Sou Lee <Sou_Lee92@yahoo.com>
2021-04-06 18:57:12 -0700

    Fix Greeting Container to save currentUser as a prop

--- frontend/components/navbar/greeting_container.jsx
+++ frontend/components/navbar/greeting_container.jsx
@@ -4,9 +4,9 @@ import { logout } from '../../actions/session_actions';
 import { openModal } from '../../actions/modal_actions';
 import Greeting from './greeting';
 
-const mSTP = ({ session }) => {
+const mSTP = ({ entities,session }) => {
   return {
-    currentUser: session.currentUserId
+    currentUser: entities.users[session.currentUserId]
   };
 };
 
