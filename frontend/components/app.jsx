f6f1cbab01c6a8d22f034e9006b51480580b7b64
Sou Lee <Sou_Lee92@yahoo.com>
2021-04-07 08:00:47 -0700

    Fix merge conflict

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -6,19 +6,6 @@ import SessionFormContainer from './session/signup_form_container'
 import LoginFormContainer from './session/login_form_container';
 import ModalContainer from "./modal/modal_container";
 import GreetingContainer from './navbar/greeting_container';
-import Greeting from './navbar/greeting';
-import { AuthRoute, ProtectedRoute } from '../util/route_utils'; 
 
-const App = () => (
-  <div>
-    <ModalContainer />
-    <h1>Fish N Games</h1>
-    <header>
-      <GreetingContainer />
-    </header>
-    <AuthRoute path="/signup" component={SessionFormContainer} />
-    <ProtectedRoute exact path="/login" component={LoginFormContainer} />
-  </div>
-);
 
 export default App;
