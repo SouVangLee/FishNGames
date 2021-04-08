6f31bff337b9c6cbe0848fcee31c0499f0355355
Sou Lee <Sou_Lee92@yahoo.com>
2021-04-07 21:47:45 -0700

    Update 404 with image

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import { Route, Link, Switch, Redirect } from 'react-router-dom';
-import { Provider } from 'react-redux';
 
-import SessionFormContainer from './session/signup_form_container'
-import LoginFormContainer from './session/login_form_container';
 import ModalContainer from "./modal/modal_container";
-import GreetingContainer from './navbar/greeting_container';
 import { AuthRoute, ProtectedRoute } from '../util/route_utils';
 import Navbar from './navbar/navbar';
 import Cart from './cart/cart';
@@ -19,7 +15,6 @@ const App = () => (
     <Navbar />
     <Switch>
       <ProtectedRoute exact path="/cart" component={Cart} />
-      <AuthRoute path="/signup" component={SessionFormContainer} />
       <Route exact path="/" component={Splash} />
       <Route path ="/404" component={PageNotFound}/>
       <Redirect to="404" />
--- frontend/components/page_not_found.jsx
+++ frontend/components/page_not_found.jsx
@@ -8,6 +8,9 @@ class PageNotFound extends React.Component {
         <img className="error-404" src={window.error_404}/>
         {/* <a href="https://www.freepik.com/vectors/background">
         Background vector created by freepik - www.freepik.com</a> */}
+        <div className="redirect-404">
+          <Link to="/">{">>Go to Home Page<<"}</Link>
+        </div>
       </div>
     )
   } 
