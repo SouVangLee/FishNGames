f2e597eb870355d9d37991216b2e328cc1ae9332
Sou Lee <Sou_Lee92@yahoo.com>
2021-04-08 17:26:15 -0700

    Add controller actions for products

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -1,2 +1,23 @@
 class Api::CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+    render 'api/categories/show'
+  end
+
+  def show
+    @category = Category.find_by(id: params[:id])
+    render 'api/categories/show'
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category
+      render 'api/categories/show'
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,42 @@
 class Api::ProductsController < ApplicationController
+  def show
+    @product = Product.find_by(id: params[:id])
+    render "api/products/show"
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      render "api/products/index"
+    else
+      render json: @product.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    if !@product.nil?
+      deleted_product = @product.delete
+    else
+      render json: ["Invalid product!"], status: 422
+    end
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    if @product.nil?
+      render json: ["Invalid product!"], status: 422
+    else
+      if @product.update(product_params)
+        render "api/products/show"
+      else
+        render @product.errors.full_messages, status: 422
+      end
+    end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :price, :quantity, :description, :category_id);
+  end
 end
