171c8312701d215e3d114ba3e0f89c4bebbf01d8
Sou Lee <Sou_Lee92@yahoo.com>
2021-04-13 20:15:14 -0700

    Add update controller action to review

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -19,6 +19,10 @@ class Api::ReviewsController < ApplicationController
   def update
     @review = Review.find_by(id: params[:id])
     if !@review.nil? && @review.update(review_params)
+      render '/api/reviews/index'
+    else
+      render json @review.errors.full_messages, status: 422
+    end
   end
 
   def destroy
