df015367db0ef82dad2ec97668b7e7770d2db381
Sou Lee <Sou_Lee92@yahoo.com>
2021-04-15 00:31:39 -0700

    Finish updating styling for buttons

--- frontend/components/review/review_index_item.jsx
+++ frontend/components/review/review_index_item.jsx
@@ -10,22 +10,21 @@ class ReviewItem extends React.Component {
     const date = createdAt.slice(0, 10);
     return (
       <div className="review-item-container">
+
         <nav className="review-item-nav">
           <div className="name-rating-container">
-            <h2>{name}&nbsp;&nbsp;{date}</h2>
+            <h2>{name} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {date}</h2>
             <h3 className="rating">Rating: {rating} Stars</h3>
           </div>
 
-          {/* <div className="delete-container"> */}
-            <button className="delete-review-button">Delete</button>
-          {/* </div> */}
+          <button className='review-edit-button review-cancel-button'>Edit/Cancel</button> 
         </nav>
 
           <div className="p-buttons-container">
             <p>{comment}</p>
             <div className="bottom-review-buttons">
-              <button className="edit-review-button">Edit</button>
-              <button className="cancel-edit-button">Cancel</button>
+              <button className="delete-review-button">Delete</button>
+              <button className='review-update-button'>Update</button>
             </div>
           </div>
       </div>
