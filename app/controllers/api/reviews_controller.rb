b36c473ee6ba6836555e85ed0b35ec54f5e6e3a7
Sou Lee <Sou_Lee92@yahoo.com>
2021-04-15 16:36:47 -0700

    test

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,5 +1,5 @@
 class Api::ReviewsController < ApplicationController
-  before_action :require_logged_in, only: [:create, :update, :destroy]
+  # before_action :require_logged_in, only: [:create, :update, :destroy]
 
   def index
     @reviews = Review.all
