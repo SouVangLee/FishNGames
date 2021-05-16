59eb09da1640e4b0edc02a5d63fe3570ed25af78
Sou Lee <Sou_Lee92@yahoo.com>
2021-05-16 16:47:22 -0700

    Add header for reviews

---
+++ db/migrate/20210516234519_add_header_to_reviews.rb
@@ -0,0 +1,5 @@
+class AddHeaderToReviews < ActiveRecord::Migration[5.2]
+  def change
+    add_column :reviews, :header, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_14_024809) do
+ActiveRecord::Schema.define(version: 2021_05_16_234519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2021_04_14_024809) do
     t.text "comment", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "header"
     t.index ["product_id"], name: "index_reviews_on_product_id"
     t.index ["reviewer_id"], name: "index_reviews_on_reviewer_id"
   end
