5745573b176cce7bb54ff6930116c3f4652bb836
Sou Lee <Sou_Lee92@yahoo.com>
2021-05-28 19:15:41 -0700

    Add update action for cart items

--- app/controllers/api/cart_items_controller.rb
+++ app/controllers/api/cart_items_controller.rb
@@ -16,23 +16,37 @@ class Api::CartItemsController < ApplicationController
       cart_items.each do |cart_item|
         product_ids[cart_item.product_id] = cart_item.id
       end
-
+      NEW_QUANTITY = @cart_item.quantity + @existing_item.quantity
       @existing_item = CartItem.find_by(id: product_ids[@cart_item.product_id])
-      if @existing_item.update(
+      if @existing_item && @existing_item.update(
         user_id: @existing_item.user_id, 
         product_id: @existing_item.product_id,
-        quantity: @cart_item.quantity
+        quantity: NEW_QUANTITY
       )
+        @cart_items = CartItem.all.select { |cart_item| cart_item.user_id == current_user.id }
         render "api/cart_items/index"
       else
         render json @existing_item.errors.full_messages, status: 422
       end
     else
-      if @cart_item.save
+      if current_user && @cart_item.save
+        @cart_items = CartItem.all.select { |cart_item| cart_item.user_id == current_user.id }
         render "api/cart_items/index"
       else
         render json @cart_item.errors.full_messages, status: 422
       end
+    end
+  end
+
+  def update
+    if current_user
+      @cart_item = CartItem.find_by(id: params[:id])
+      if @cart_item && @cart_item.update(cart_item_params)
+        @cart_items = CartItem.all.select { |cart_item| cart_item.user_id == current_user.id }
+        render "api/cart_items/index"
+      end
+    else
+      render json: ["You must be signed in!"], status: 404
     end
   end
 
