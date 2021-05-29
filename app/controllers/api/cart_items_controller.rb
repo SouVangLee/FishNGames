61dae19f6a0cb318214748ad10a3f6540b32054f
Sou Lee <Sou_Lee92@yahoo.com>
2021-05-28 18:58:30 -0700

    Add create action for cart items

--- app/controllers/api/cart_items_controller.rb
+++ app/controllers/api/cart_items_controller.rb
@@ -1,13 +1,38 @@
 class Api::CartItemsController < ApplicationController
   def index
-    @cart_items = CartItem.all.select {|cart_item| cart_item.user_id == current_user.id}
-    render "api/cart_items/index"
+    if current_user
+      @cart_items = CartItem.all.select { |cart_item| cart_item.user_id == current_user.id }
+      render "api/cart_items/index"
+    else
+      return nil
+    end
   end
 
   def create
+    cart_items = CartItem.all.select { |cart_item| cart_item.user_id == current_user.id }
     @cart_item = CartItem.new(product_params)
-    if @cart_item.save
-      render "api/"
+    if current_user && cart_items.any? { |item| @cart_item.product_id == item.product_id }
+      product_ids = {}
+      cart_items.each do |cart_item|
+        product_ids[cart_item.product_id] = cart_item.id
+      end
+
+      @existing_item = CartItem.find_by(id: product_ids[@cart_item.product_id])
+      if @existing_item.update(
+        user_id: @existing_item.user_id, 
+        product_id: @existing_item.product_id,
+        quantity: @cart_item.quantity
+      )
+        render "api/cart_items/index"
+      else
+        render json @existing_item.errors.full_messages, status: 422
+      end
+    else
+      if @cart_item.save
+        render "api/cart_items/index"
+      else
+        render json @cart_item.errors.full_messages, status: 422
+      end
     end
   end
 
