11304cd2a2d4e9f68227aacc632020529f632cdb
Sou Lee <Sou_Lee92@yahoo.com>
2021-05-28 19:57:36 -0700

    Add cart item actions

---
+++ frontend/actions/cart_item_actions.js
@@ -0,0 +1,24 @@
+import * as CartItemApiUtil from '../util/cart_item_utils'
+
+export const RECEIVE_ALL_CART_ITEMS = "RECEIVE_ALL_CART_ITEMS"
+export const REMOVE_CART_ITEM = "REMOVE_CART_ITEM"
+
+const receiveAllCartItems = cartItems => ({
+  type: RECEIVE_ALL_CART_ITEMS,
+  cartItems
+});
+
+const removeCartItem = cartItemId => ({
+  type: REMOVE_CART_ITEM,
+  cartItemId
+});
+
+export const fetchAllCartItems = () => dispatch => {
+  return CartItemApiUtil.fetchAllCartItems()
+    .then(cartItems => dispatch(receiveAllCartItems(cartItems)));
+};
+
+export const deleteCartItem = cartItemId => dispatch => {
+  return CartItemApiUtil.deleteCartItem(cartItemId)
+    .then(() => dispatch(removeCartItem(cartItemId)));
+};
