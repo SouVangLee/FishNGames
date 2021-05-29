6a2743a5a9ccb09a3b9e0218859d49a659839b9c
Sou Lee <Sou_Lee92@yahoo.com>
2021-05-28 20:14:03 -0700

    Add cart items reducer

---
+++ frontend/reducers/cart_items_reducer.js
@@ -0,0 +1,20 @@
+import {
+  RECEIVE_ALL_CART_ITEMS,
+  REMOVE_CART_ITEM
+} from '../actions/cart_item_actions';
+
+const CartItemReducer = (state = {}, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_ALL_CART_ITEMS:
+      return Object.assign({}, action.cartItems)
+    case REMOVE_CART_ITEM:
+      const newState = Object.assign({}, state);
+      delete newState[action.cartItemId];
+      return newState;
+    default:
+      return {...state};
+  }
+};
+
+export default CartItemReducer;
