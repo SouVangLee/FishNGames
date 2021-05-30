bc9581116c0e76cbf9714eb24f3b55f80b420d87
Sou Lee <Sou_Lee92@yahoo.com>
2021-05-29 21:36:33 -0700

    Add cart item errors action

--- frontend/actions/cart_item_actions.js
+++ frontend/actions/cart_item_actions.js
@@ -1,7 +1,8 @@
 import * as CartItemApiUtil from '../util/cart_item_utils'
 
-export const RECEIVE_ALL_CART_ITEMS = "RECEIVE_ALL_CART_ITEMS"
-export const REMOVE_CART_ITEM = "REMOVE_CART_ITEM"
+export const RECEIVE_ALL_CART_ITEMS = "RECEIVE_ALL_CART_ITEMS";
+export const REMOVE_CART_ITEM = "REMOVE_CART_ITEM";
+export const RECEIVE_CART_ITEM_ERRORS = "RECEIVE_CART_ITEM_ERRORS";
 
 const receiveAllCartItems = cartItems => ({
   type: RECEIVE_ALL_CART_ITEMS,
@@ -13,22 +14,39 @@ const removeCartItem = cartItemId => ({
   cartItemId
 });
 
+export const receiveErrors = errors => ({
+  type: RECEIVE_CART_ITEM_ERRORS,
+  errors
+});
+
 export const fetchAllCartItems = () => dispatch => {
   return CartItemApiUtil.fetchAllCartItems()
-    .then(cartItems => dispatch(receiveAllCartItems(cartItems)));
+    .then(
+      cartItems => dispatch(receiveAllCartItems(cartItems)),
+      errors => dispatch(receiveErrors(errors.responseJSON))
+    );
 };
 
 export const createCartItem = cartItem => dispatch => {
   return CartItemApiUtil.createCartItem(cartItem)
-    .then(cartItems => dispatch(receiveAllCartItems(cartItems)));
+    .then(
+      cartItems => dispatch(receiveAllCartItems(cartItems)),
+      errors => dispatch(receiveErrors(errors.responseJSON))
+    );
 };
 
 export const updateCartItem = cartItem => dispatch => {
   return CartItemApiUtil.updateCartItem(cartItem)
-    .then(cartItems => dispatch(receiveAllCartItems(cartItems)));
+    .then(
+      cartItems => dispatch(receiveAllCartItems(cartItems)),
+      errors => dispatch(receiveErrors(errors.responseJSON))  
+    );
 };
 
 export const deleteCartItem = cartItemId => dispatch => {
   return CartItemApiUtil.deleteCartItem(cartItemId)
-    .then(() => dispatch(removeCartItem(cartItemId)));
+    .then(
+      () => dispatch(removeCartItem(cartItemId)),
+      errors => dispatch(receiveErrors(errors.responseJSON))
+    );
 };
